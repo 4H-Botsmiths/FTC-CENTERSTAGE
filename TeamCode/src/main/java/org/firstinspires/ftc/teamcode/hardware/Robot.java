b44ae752a3804516e0c2b963874199a3b2ae2ddb
Kolton Bloom <bloomkd46@icloud.com>
2023-12-17 12:50:43 -0800

    Added lift class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
@@ -4,6 +4,9 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import java.util.concurrent.CompletableFuture;
 import org.firstinspires.ftc.teamcode.classes.DCMotor;
 
 public class Robot {
@@ -29,6 +32,8 @@ public class Robot {
   /** Port 2 ? */
   public Servo rightElbow = null;
 
+  public Lift lift = null;
+
   public Robot(HardwareMap hardwareMap) {
     frontLeft = new DCMotor(hardwareMap.get(DcMotorEx.class, "FrontLeft"));
     frontRight = new DCMotor(hardwareMap.get(DcMotorEx.class, "FrontRight"), DcMotor.Direction.REVERSE);
@@ -44,5 +49,133 @@ public class Robot {
     leftElbow.setDirection(Servo.Direction.REVERSE);
     leftElbow.scaleRange(0.03, 0.4);
     rightElbow.scaleRange(0.03, 0.4);
+
+    lift = new Lift(leftRiser, rightRiser);
+  }
+
+  public enum LiftStatus {
+    RAISED, RAISING, LOWERED, LOWERING
+  }
+
+  public class Lift {
+    DCMotor leftMotor;
+    DCMotor rightMotor;
+
+    public Lift(DCMotor left, DCMotor right) {
+      this.leftMotor = left;
+      this.rightMotor = right;
+    }
+
+    public LiftStatus status = LiftStatus.LOWERED;
+    private ElapsedTime liftClock = new ElapsedTime();
+    private final int DURATION = 1750;
+
+    private double calcSpeed(double time) {
+      return Range.clip(time < 0.5 * DURATION ? 1 : (DURATION - time) / DURATION, 0, 1);
+    }
+
+    private double calcSpeed(double time, double duration) {
+      return Range.clip(time < 0.5 * duration ? 1 : (duration - time) / duration, 0, 1);
+    }
+
+    public void raise() {
+      if (status == LiftStatus.RAISED || status == LiftStatus.RAISING) {
+        return;
+      }
+      if (status == LiftStatus.LOWERING) {
+        double raiseTime = liftClock.milliseconds();
+        status = LiftStatus.RAISING;
+        liftClock.reset();
+        CompletableFuture.runAsync(() -> {
+          while (liftClock.milliseconds() < raiseTime && status == LiftStatus.RAISING) {
+            double time = liftClock.milliseconds();
+            double speed = calcSpeed(time, raiseTime);
+            leftMotor.setSpeed(speed);
+            rightMotor.setSpeed(speed);
+            try {
+              Thread.sleep(20); // Sleep for 20 milliseconds
+            } catch (InterruptedException e) {
+              // Handle interrupted exception
+            }
+          }
+          if (status == LiftStatus.RAISING) {
+            leftMotor.setSpeed(0);
+            rightMotor.setSpeed(0);
+            status = LiftStatus.RAISED;
+          }
+        });
+      } else {
+        status = LiftStatus.RAISING;
+        liftClock.reset();
+        CompletableFuture.runAsync(() -> {
+          while (liftClock.milliseconds() < DURATION && status == LiftStatus.RAISING) {
+            double time = liftClock.milliseconds();
+            double speed = calcSpeed(time);
+            leftMotor.setSpeed(speed);
+            rightMotor.setSpeed(speed);
+            try {
+              Thread.sleep(20); // Sleep for 20 milliseconds
+            } catch (InterruptedException e) {
+              // Handle interrupted exception
+            }
+          }
+          if (status == LiftStatus.RAISING) {
+            leftMotor.setSpeed(0);
+            rightMotor.setSpeed(0);
+            status = LiftStatus.RAISED;
+          }
+        });
+      }
+    }
+
+    public void lower() {
+      if (status == LiftStatus.LOWERED || status == LiftStatus.LOWERING) {
+        return;
+      }
+      if (status == LiftStatus.RAISING) {
+        double lowerTime = liftClock.milliseconds();
+        status = LiftStatus.LOWERING;
+        liftClock.reset();
+        CompletableFuture.runAsync(() -> {
+          while (liftClock.milliseconds() < lowerTime && status == LiftStatus.LOWERING) {
+            double time = liftClock.milliseconds();
+            double speed = -calcSpeed(time, lowerTime);
+            leftMotor.setSpeed(speed);
+            rightMotor.setSpeed(speed);
+            try {
+              Thread.sleep(20); // Sleep for 20 milliseconds
+            } catch (InterruptedException e) {
+              // Handle interrupted exception
+            }
+          }
+          if (status == LiftStatus.LOWERING) {
+            leftMotor.setSpeed(0);
+            rightMotor.setSpeed(0);
+            status = LiftStatus.LOWERED;
+          }
+        });
+      } else {
+        status = LiftStatus.LOWERING;
+        liftClock.reset();
+        CompletableFuture.runAsync(() -> {
+          while (liftClock.milliseconds() < DURATION && status == LiftStatus.LOWERING) {
+            double time = liftClock.milliseconds();
+            double speed = -calcSpeed(time);
+            leftMotor.setSpeed(speed);
+            rightMotor.setSpeed(speed);
+            try {
+              Thread.sleep(20); // Sleep for 20 milliseconds
+            } catch (InterruptedException e) {
+              // Handle interrupted exception
+            }
+          }
+          if (status == LiftStatus.LOWERING) {
+            leftMotor.setSpeed(0);
+            rightMotor.setSpeed(0);
+            status = LiftStatus.LOWERED;
+          }
+        });
+      }
+    }
   }
 }
