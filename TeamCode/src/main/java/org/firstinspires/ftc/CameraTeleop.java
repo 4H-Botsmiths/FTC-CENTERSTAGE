0299c505a1b4445b44e819098fbc5215b151a2cb
Kolton Bloom <bloomkd46@icloud.com>
2023-12-17 15:50:17 -0800

    fixed typo

--- TeamCode/src/main/java/org/firstinspires/ftc/CameraTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/CameraTeleop.java
@@ -348,7 +348,7 @@ public class CameraTeleop extends OpMode {
     telemetry.addData("Status", "Running");
     telemetry.addData("Voltage", Math.round(robot.voltage.getVoltage() * 100) / 100);
     telemetry.addData("Performance", Math.round(robot.performance * 100));
-    telemetry.addData("Average Voltage", Math.round(robot.a * 100) / 100);
+    telemetry.addData("Average Voltage", Math.round(robot.averageVoltage * 100) / 100);
     telemetry.addData("Average Performance", Math.round(robot.averagePerformance * 100));
     telemetry.addData("Front Left", Math.round(robot.frontLeft.getSpeed() * 100));
     telemetry.addData("Front Right", Math.round(robot.frontRight.getSpeed() * 100));
