837dde61e8429046dc50b5c018378cb47dc45266
Kolton Bloom <bloomkd46@icloud.com>
2024-01-13 09:18:27 -0800

    Finalized for backstage?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/AutoGlobals.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/AutoGlobals.java
@@ -4,19 +4,21 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class AutoGlobals {
+  public static final double PRIMARY_SPEED = 0.2;
+  public static final double SECONDARY_SPEED = 0.15;
 
   /** How long in milliseconds to delay before starting to move - make sure it still allows time to place on board and get backstage */
   public static final int DELAY_DURATION = 1000;
-  public static final int STRAFE_DURATION = 1000;
-  public static final int PLACE_DURATION = 3000;
-  public static final int BACKUP_DURATION = 500;
-  public static final int PARKING_STRAFE_DURATION = 1000;
-  public static final int PARKING_DURATION = 1000;
+  public static final int STRAFE_DURATION = 3000;
+  public static final int PLACE_DURATION = 2000;
+  public static final int BACKUP_DURATION = 1000;
+  public static final int PARKING_STRAFE_DURATION = 6000;
+  public static final int PARKING_DURATION = 6000;
 
   /*Use commented code below as a template, but this function will not receive any input from the gamepads and will need to know that pixels have been placed 1000 milliseconds after entering the placing phase*/
   // degrees are more sensitive than inches, so they need finer control
-  public static final double turnSensitivity = 0.05;
-  public static final double sensitivity = 0.075;
+  public static final double turnSensitivity = 0.025;
+  public static final double sensitivity = 0.05;
   public static final double speedLimit = 0.15;
   public static final double turnSpeedLimit = 0.1;
   public static final double precisionSpeedLimit = 0.15;
