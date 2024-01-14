37d8d726f7eeafc1ccca0f019ac18e26a7658840
Kolton Bloom <bloomkd46@icloud.com>
2024-01-14 11:24:41 -0800

    Added some new variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/AutoGlobals.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/AutoGlobals.java
@@ -6,6 +6,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class AutoGlobals {
   public static final double PRIMARY_SPEED = 0.2;
   public static final double SECONDARY_SPEED = 0.15;
+  public static final double TERTIARY_SPEED = 0.3;
 
   /** How long in milliseconds to delay before starting to move - make sure it still allows time to place on board and get backstage */
   public static final int DELAY_DURATION = 1000;
@@ -14,6 +15,8 @@ public class AutoGlobals {
   public static final int BACKUP_DURATION = 1000;
   public static final int PARKING_STRAFE_DURATION = 6000;
   public static final int PARKING_DURATION = 6000;
+
+  public static final int CREEP_DURATION = 1000;
 
   /*Use commented code below as a template, but this function will not receive any input from the gamepads and will need to know that pixels have been placed 1000 milliseconds after entering the placing phase*/
   // degrees are more sensitive than inches, so they need finer control
@@ -36,7 +39,12 @@ public class AutoGlobals {
   public final static double PARKING_X_DISTANCE = 3;
 
   /** How long the robot should strafe for to get to the center of the field  */
-  public final static int ONSTAGE_STRAFE_DURATION = 1000;
+  public final static int ONSTAGE_STRAFE_DURATION = 4000;
   /** How long the robot should drive forward to get backstage */
-  public final static int ONSTAGE_DRIVE_DURATION = 1000;
+  public final static int ONSTAGE_DRIVE_DURATION = 4000;
+
+  public final static double POPUP_SPEED = 0.2;
+  public final static int POPUP_DURATION = 500;
+
+  public final static int LETS_BACK_UP = 6000;
 }
